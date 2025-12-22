c7f4e7f834b07a4d2b35e65ce0d2c1e7e1e950ce
fhmyazz <fahmyaziz1805@gmail.com>
2025-12-22 23:38:30 +0700

    fixing token logic

--- src/db/auth.db.js
+++ src/db/auth.db.js
@@ -3,7 +3,7 @@ const tokenGenerator = (length) => {
     let result = ""
 
     for (let i = 0; i < length; i ++){
-        result += characters.charAt(Math.floor(Math.random() * length))
+        result += characters.charAt(Math.floor(Math.random() * characters.length))
     }
     return result
 }
